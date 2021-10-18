540b25985506b488e4b926644b1b562b507f190a
Taylor Dean <taylordean787@gmail.com>
2021-10-18 09:20:55 -0500

    update logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { loadHome } from "../modules/pageload.js";
-
-console.log("hello");
-
+import { loadMenu } from "../modules/loadMenu.js";
+import { loadHome } from "../modules/loadHome.js";
+import { loadContact } from "../modules/loadContact.js";
 loadHome();
+
+document.addEventListener("click", (e) => {
+  console.log(e.target.innerText);
+
+  let clicked = e.target.innerText;
+
+  if (clicked === "Menu") {
+    loadMenu();
+  } else if (clicked === "Home") {
+    loadHome();
+  } else if (clicked === "Contact") {
+    loadContact();
+  }
+});
